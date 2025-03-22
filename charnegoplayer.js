a85d565e7f0e19dc8d00bf2b1e0818e6331ad263
Krusher <axelei@gmail.com>
2025-03-22 22:29:58 +0100

    Más juegos y cosas

--- charnegoplayer.js
+++ charnegoplayer.js
@@ -1,14 +1,39 @@
 const juegos = {
 	cerdadx : {
 		gameName : 'La Lellenda de la Cerda',
-		gameUrl : 'juegos/cerdadx.zip',
+		gameUrl : 'juegos/La Lellenda de la Cerda DX (2.0).zip',
 		core : 'gb',
-	}
+	},	
+	plashback : {
+		gameName : 'Plashback',
+		gameUrl : 'juegos/Plashback.zip',
+		core : 'snes',
+	},
+	stressf2 : {
+		gameName : 'Super Stress Faijter II',
+		gameUrl : 'juegos/Super Stress Faijter II.zip',
+		core : 'snes',
+	},
+	kombatemortas : {
+		gameName : 'Kombate Morta',
+		gameUrl : 'juegos/Kombate Morta.zip',
+		core : 'segaMD',
+	},
+	chforce2 : {
+		gameName : 'Charnego Force II',
+		gameUrl : 'juegos/Charnego Force II.zip',
+		core : 'segaMD',
+	},
+	tecmoatleti : {
+		gameName : 'Tecmo Atleti',
+		gameUrl : 'juegos/Tecmo Atleti.zip',
+		core : 'segaMD',
+	},
 };
 			
-const hash = window.location.hash.substring(1);
+const juego = (new URLSearchParams(window.location.search)).get('juego');
 
-if (hash == '') {
+if (juego == '') {
 	alert("¡¡Pero qué estás hasiendo con el cuerpo humano!!");
 	throw new Error('Yes we Jarl');
 }
@@ -20,13 +45,13 @@ function onReady() {
 }
 
 EJS_player = "#player";
-EJS_core = juegos[hash].core;
+EJS_core = juegos[juego].core;
 EJS_language = 'es-ES';
-EJS_gameName = juegos[hash].gameName;
+EJS_gameName = juegos[juego].gameName;
 EJS_color = "#0064ff";
 EJS_startOnLoaded = true;
 EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
-EJS_gameUrl = juegos[hash].gameUrl;
+EJS_gameUrl = juegos[juego].gameUrl;
 EJS_onGameStart = onReady;
 
 window.loadingmodal.showModal();
